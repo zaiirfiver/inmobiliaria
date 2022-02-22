38913d1711e80d8e6e5dca0e8a632a21b9ecd9f6
Zaira Popoca <zairapl12@gmail.com>
2022-02-22 15:08:06 -0600

    funcionalidad vista de agenda

--- src/main/java/com/zmlc/inmobiliaria/controller/HomeController.java
+++ src/main/java/com/zmlc/inmobiliaria/controller/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.zmlc.inmobiliaria.model.Inmueble;
@@ -41,5 +42,10 @@ public class HomeController {
 		model.addAttribute("inmueble", inmueble);
 		
 		return "usuario/inmueblehome";
+	}
+	
+	@PostMapping("/schedule")
+	public String addSchedule() {
+		return "usuario/agenda";
 	}
 }
