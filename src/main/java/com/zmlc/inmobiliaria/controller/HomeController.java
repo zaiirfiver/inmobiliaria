dfb2ff8c0e2569958d1ed7806e0770ccbee23b5b
Zaira Popoca <zairapl12@gmail.com>
2022-02-22 12:25:52 -0600

    mostrar información del inmueble

--- src/main/java/com/zmlc/inmobiliaria/controller/HomeController.java
+++ src/main/java/com/zmlc/inmobiliaria/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.zmlc.inmobiliaria.controller;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.zmlc.inmobiliaria.model.Inmueble;
 import com.zmlc.inmobiliaria.service.InmuebleService;
 
 @Controller
@@ -29,8 +32,14 @@ public class HomeController {
 	}
 	
 	@GetMapping("inmueblehome/{id}")
-	public String inmuebleHome(@PathVariable Integer id) {
+	public String inmuebleHome(@PathVariable Integer id, Model model) {
 		log.info("id enviado como parametro {}", id);
+		Inmueble inmueble = new Inmueble();
+		Optional<Inmueble> inmuebleOptional= inmuebleService.get(id);
+		inmueble = inmuebleOptional.get();
+		
+		model.addAttribute("inmueble", inmueble);
+		
 		return "usuario/inmueblehome";
 	}
 }
