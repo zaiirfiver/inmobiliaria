8b7f6f9513629936f25a2dc0129140dba40d86e5
Zaira Popoca <zairapl12@gmail.com>
2022-02-22 11:17:26 -0600

    funcionalidad para ver inmueble para el usuario

--- src/main/java/com/zmlc/inmobiliaria/controller/HomeController.java
+++ src/main/java/com/zmlc/inmobiliaria/controller/HomeController.java
@@ -1,9 +1,12 @@
 package com.zmlc.inmobiliaria.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.zmlc.inmobiliaria.service.InmuebleService;
@@ -11,6 +14,8 @@ import com.zmlc.inmobiliaria.service.InmuebleService;
 @Controller
 @RequestMapping("/")
 public class HomeController {
+	
+	private final Logger log= LoggerFactory.getLogger(HomeController.class);
 	
 	@Autowired
 	private InmuebleService inmuebleService;
@@ -21,5 +26,11 @@ public class HomeController {
 		model.addAttribute("inmuebles", inmuebleService.findAll());
 		
 		return "usuario/home";
+	}
+	
+	@GetMapping("inmueblehome/{id}")
+	public String inmuebleHome(@PathVariable Integer id) {
+		log.info("id enviado como parametro {}", id);
+		return "usuario/inmueblehome";
 	}
 }
