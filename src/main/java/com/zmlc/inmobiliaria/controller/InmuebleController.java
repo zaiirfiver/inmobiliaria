e4bcbd0532c0ce9b6e392a128cd9d5ec70d7458e
Zaira Popoca <zairapl12@gmail.com>
2022-02-22 05:01:18 -0600

    funcionalidad para guardar imagenes

--- src/main/java/com/zmlc/inmobiliaria/controller/InmuebleController.java
+++ src/main/java/com/zmlc/inmobiliaria/controller/InmuebleController.java
@@ -1,5 +1,6 @@
 package com.zmlc.inmobiliaria.controller;
 
+import java.io.IOException;
 import java.util.Optional;
 
 import org.slf4j.*;
@@ -10,10 +11,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.zmlc.inmobiliaria.model.Inmueble;
 import com.zmlc.inmobiliaria.model.Usuario;
 import com.zmlc.inmobiliaria.service.InmuebleService;
+import com.zmlc.inmobiliaria.service.UploadFileService;
 
 @Controller
 @RequestMapping("/inmuebles")
@@ -23,6 +27,9 @@ public class InmuebleController {
 	
 	@Autowired
 	private InmuebleService inmuebleService;
+	
+	@Autowired
+	private UploadFileService upload;
 	
 	@GetMapping("")
 	public String show(Model model) {
@@ -36,10 +43,27 @@ public class InmuebleController {
 	}
 	
 	@PostMapping("/save")
-	public String save(Inmueble inmueble) {
+	public String save(Inmueble inmueble, @RequestParam("img") MultipartFile file) throws IOException {
 		LOGGER.info("Este es el objeto inmueble {}", inmueble);
 		Usuario u= new Usuario(1, "", "", "", "", "", "");
 		inmueble.setUsuario(u);
+		
+		//imagen
+		if (inmueble.getId()==null) { 	//cuando se crea un inmueble
+			String nombreImagen = upload.saveImage(file);
+			inmueble.setImagen(nombreImagen);
+		} else {
+			if (file.isEmpty()) { 		//editar el inmueble pero no se cambia la imagen
+				Inmueble i = new Inmueble();
+				i = inmuebleService.get(inmueble.getId()).get();
+				inmueble.setImagen(i.getImagen());
+			}else {
+				String nombreImagen = upload.saveImage(file);
+				inmueble.setImagen(nombreImagen);
+			}
+		}
+		
+		
 		inmuebleService.save(inmueble);
 		return "redirect:/inmuebles";
 	}
